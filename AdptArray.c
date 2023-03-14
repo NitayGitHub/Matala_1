c52d5d69e4e8a88e2be19a30c597457873760926
nitayGitHub <nitaylevi1998@gmail.com>
2023-03-14 19:35:13 +0200

    firstcomit

---
+++ AdptArray.c
@@ -0,0 +1,10 @@
+#include "AdptArray.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+PAdptArray CreateAdptArray(COPY_FUNC, DEL_FUNC,PRINT_FUNC){
+    
+}
