527de39f28795f2bc007b07006865cc0612e8ffd
nitayGitHub <nitaylevi1998@gmail.com>
2023-03-14 20:45:39 +0200

    adpt1

--- AdptArray.c
+++ AdptArray.c
@@ -5,6 +5,17 @@
 #include <string.h>
 #include <errno.h>
 
-PAdptArray CreateAdptArray(COPY_FUNC, DEL_FUNC,PRINT_FUNC){
-    
+typedef struct AdptArray_{
+    PElement* PElemArr;
+    int arr_size;
+    COPY_FUNC copy_func;
+    DEL_FUNC del_func;
+    PRINT_FUNC print_func;
+} AdptArr, * PAdptArray;
+
+PAdptArray CreateAdptArray(COPY_FUNC cf, DEL_FUNC df,PRINT_FUNC pf){
+    PAdptArray pa = (PAdptArray)malloc(sizeof(AdptArr));
+    pa->arr_size = 0;
+    pa->PElemArr = (PElement*)malloc(sizeof(PElement));
+    pa->copy_func = &cf;
 }
