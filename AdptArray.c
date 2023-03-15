c754f8b983e4e5c56fd3ba84be5f289962cb0e02
nitayGitHub <nitaylevi1998@gmail.com>
2023-03-15 23:17:15 +0200

    last_udp

--- AdptArray.c
+++ AdptArray.c
@@ -1,34 +1,34 @@
 #include "AdptArray.h"
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
 #include <errno.h>
 
-#define INITIAL_SIZE 10
-#define GROWTH_FACTOR 3
-
-typedef struct AdptArray_{
-    PElement* PElemArr;
-    int arr_length;
+typedef struct AdptArray_
+{
+    PElement *PElemArr;
+    int num_of_elements;
     int arr_cap;
     COPY_FUNC copy_func;
     DEL_FUNC del_func;
     PRINT_FUNC print_func;
-} AdptArr, * PAdptArray;
+} AdptArr, *PAdptArray;
 
-PAdptArray CreateAdptArray(COPY_FUNC cf, DEL_FUNC df,PRINT_FUNC pf){
+PAdptArray CreateAdptArray(COPY_FUNC cf, DEL_FUNC df, PRINT_FUNC pf)
+{
     PAdptArray pa = (PAdptArray)malloc(sizeof(AdptArr));
-    if(pa == NULL){
+    if (pa == NULL)
+    {
         perror("Error allocating PAdptArray memory.\n");
-        return NULL;
+        return FAIL;
     }
-    pa->arr_length = 0;
-    pa->arr_cap = INITIAL_SIZE;
-    pa->PElemArr = (PElement*)calloc(INITIAL_SIZE, sizeof(PElement));
-    if(pa->PElemArr == NULL){
+    pa->num_of_elements = 0;
+    pa->arr_cap = 1;
+    pa->PElemArr = (PElement *)malloc(pa->arr_cap * sizeof(PElement));
+    pa->PElemArr[0] = NULL;
+    if (pa->PElemArr == NULL)
+    {
         perror("Error allocating PElemArr memory.\n");
-        return NULL;
+        return FAIL;
     }
     pa->copy_func = cf;
     pa->del_func = df;
@@ -36,41 +36,120 @@ PAdptArray CreateAdptArray(COPY_FUNC cf, DEL_FUNC df,PRINT_FUNC pf){
     return pa;
 }
 
-void DeleteAdptArray(PAdptArray pa){
-    if(pa == NULL){
-        perror("PAdptArray is NULL. No action was made.\n");
-        return NULL;
-    }
-    PElement * curr = pa->PElemArr;
-    PElement * pre;
-    int length = pa->arr_length;
-    for(int i = 0; i < length; i++)
+void DeleteAdptArray(PAdptArray pa)
+{
+    if (pa == NULL)
     {
-        pre = curr;
-        curr = curr + 1;
-        pa->del_func(*pre);
-        pre = NULL;
+        perror("PAdptArray is NULL. No action was made.\n");
+        return;
     }
+    PElement curr;
+    int length = pa->arr_cap;
+    for (int i = 0; i < length; i++)
+    {
+        curr = pa->PElemArr[i];
+        if (curr)
+        {
+            pa->del_func(curr);
+        }
+    }
+    free(pa->PElemArr);
     free(pa);
 }
 
-Result SetAdptArrayAt(PAdptArray pa, int indx, PElement val){
-    if (pa == NULL){
+Result SetAdptArrayAt(PAdptArray pa, int indx, PElement val)
+{
+    if (pa == NULL)
+    {
+        perror("PAdptArray is NULL. Please create an array first.\n");
+        return FAIL;
+    }
+    if (indx < 0)
+    {
+        perror("Index must be a positive.\n");
+        return FAIL;
+    }
+    if (indx >= pa->arr_cap)
+    {
+        int pre_cap = pa->arr_cap;
+        pa->arr_cap = indx + 1;
+        pa->PElemArr = (PElement *)realloc(pa->PElemArr, pa->arr_cap * sizeof(PElement));
+        if (pa->PElemArr == NULL)
+        {
+            perror("Couldn't realloc PElemArr memory.\n");
+            return FAIL;
+        }
+        for(int i = pre_cap; i < pa->arr_cap; i++)
+        {
+            pa->PElemArr[i] = NULL;
+        }
+    }
+
+    if (pa->PElemArr[indx])
+    {
+        pa->del_func(pa->PElemArr[indx]);
+        pa->PElemArr[indx] = NULL;
+        pa->num_of_elements--;
+    }
+    pa->PElemArr[indx] = pa->copy_func(val);
+    if (pa->PElemArr[indx] == NULL)
+    {
+        perror("Couldn't copy the value.\n");
+        return FAIL;
+    }
+    pa->num_of_elements++;
+    return SUCCESS;
+}
+
+PElement GetAdptArrayAt(PAdptArray pa, int indx)
+{
+    if (pa == NULL)
+    {
         perror("PAdptArray is NULL. Please create an array first.\n");
         return NULL;
     }
-    if (indx < 0){
+    if (indx < 0)
+    {
         perror("Index must be a positive.\n");
-        return NULL;
+        return FAIL;
     }
-    if (indx >= pa->arr_cap){
-        pa->arr_cap = indx * GROWTH_FACTOR;
-        pa->PElemArr = (PElement*)realloc(pa->PElemArr, pa->arr_cap * sizeof(PElement));
-        if(pa->PElemArr == NULL){
-            perror("Error allocating PElemArr memory.\n");
-            return NULL;
-        }
-        
+    if (indx >= pa->arr_cap)
+    {
+        perror("Index out of range.\n");
+        return FAIL;
     }
+    if (pa->PElemArr[indx])
+    {
+        return pa->copy_func(pa->PElemArr[indx]);
+    }
+    return NULL;
+}
 
+int GetAdptArraySize(PAdptArray pa)
+{
+    if (pa == NULL)
+    {
+        perror("PAdptArray is NULL. Please create an array first.\n");
+        return -1;
+    }
+    return pa->arr_cap;
+}
+
+void PrintDB(PAdptArray pa)
+{
+    if (pa == NULL)
+    {
+        perror("PAdptArray is NULL and has no elements.\n");
+        return;
+    }
+    PElement curr;
+    int length = pa->arr_cap;
+    for (int i = 0; i < length; i++)
+    {
+        curr = pa->PElemArr[i];
+        if (curr)
+        {
+            pa->print_func(curr);
+        }
+    }
 }
